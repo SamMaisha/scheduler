0d38dd39d99dd729973eaf5134782e8211be1357
Sam Maisha <sam.maisha99@gmail.com>
2023-03-26 00:02:42 -0400

    add onclick event handler to <li>

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,10 +2,10 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 
 const InterviewerListItem = (props) => {
-  const { id, name, avatar } = props;
+  const { id, name, avatar, setInterviewer } = props;
 
   return (
-    <li className="interviewers__item">
+    <li className="interviewers__item" onClick={() => setInterviewer(id)}>
       <img
         className="interviewers__item-image"
         src={avatar}
