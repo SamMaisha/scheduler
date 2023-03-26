25ddf136e783894eeaee0b0d17eaa5ec6bccc362
Sam Maisha <sam.maisha99@gmail.com>
2023-03-25 22:39:52 -0400

    render list of days within DayList component

--- src/components/Daylist.js
+++ src/components/Daylist.js
@@ -1,8 +1,22 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-const DayList = () => {
-  return <ul></ul>;
+const DayList = (props) => {
+  const { days, day, setDay } = props;
+
+  const dayListArray = days.map((x) => {
+    return (
+      <DayListItem
+        key={x.id}
+        name={x.name}
+        spots={x.spots}
+        selected={x.name === day}
+        setDay={setDay}
+      />
+    );
+  });
+
+  return <ul>{dayListArray}</ul>;
 };
 
 export default DayList;
