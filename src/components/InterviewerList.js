94c37ffee5a842de13b09406c4143c2636875cef
Sam Maisha <sam.maisha99@gmail.com>
2023-03-26 00:40:13 -0400

    return list of InterviewerListItems inside component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 const InterviewerList = (props) => {
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const interviewerListItemArr = interviewers.map((x) => {
+    return (
+      <InterviewerListItem
+        key={x.id}
+        id={x.id}
+        name={x.name}
+        avatar={x.avatar}
+        selected={x.id === interviewer}
+        setInterviewer={setInterviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewerListItemArr}</ul>
     </section>
   );
 };
