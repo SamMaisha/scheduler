e3312cd7ea2414910c94531d65253774655071aa
Sam Maisha <sam.maisha99@gmail.com>
2023-03-25 23:58:44 -0400

    replace hard coded values with props in component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,10 +8,10 @@ const InterviewerListItem = (props) => {
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
+        src={avatar}
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {name}
     </li>
   );
 };
