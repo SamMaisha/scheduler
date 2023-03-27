aaae1c38017b12ffbb3afcd0a3d32e5e3cef1a77
Sam Maisha <sam.maisha99@gmail.com>
2023-03-27 11:42:38 -0400

    refactor <Header> - pass in props

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Header = (props) => {
-  <header className="appointment__time">
-    <h4 className="text--semi-bold">12 pm</h4>
-    <hr className="appointment__separator" />
-  </header>;
+  const { time } = props;
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">{time}</h4>
+      <hr className="appointment__separator" />
+    </header>
+  );
 };
 
 export default Header;
