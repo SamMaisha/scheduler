f1e3a08a224cce812ef1e5e90941951da53ebedf
Sam Maisha <sam.maisha99@gmail.com>
2023-03-27 14:41:50 -0400

    create <Error> component; import into index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Empty from "./Empty";
 import Show from "./Show";
 import Confirm from "./Confirm";
 import Status from "./Status";
+import Error from "./Error";
 
 const Appointment = (props) => {
   return <article className="appointment"></article>;
