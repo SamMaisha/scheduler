7f7b563587714605bade3124210ea5c2c85bd92e
Sam Maisha <sam.maisha99@gmail.com>
2023-03-27 11:36:58 -0400

    creater <Header> component  and import into index.js

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = (props) => {
+  <header className="appointment__time">
+    <h4 className="text--semi-bold">12 pm</h4>
+    <hr className="appointment__separator" />
+  </header>;
+};
+
+export default Header;
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 
+import Header from "./Header";
+
 const Appointment = (props) => {
   return <article className="appointment"></article>;
 };
