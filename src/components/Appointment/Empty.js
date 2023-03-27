ea0e58b418495b25ecfda20be8ed72c76fe7b122
Sam Maisha <sam.maisha99@gmail.com>
2023-03-27 12:44:41 -0400

    refactor <Empty> component - pass in onAdd prop

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,10 +1,18 @@
 import { AddonStore } from "@storybook/addons";
 import React from "react";
 
-export default Empty = function (props) {
+const Empty = function (props) {
+  const { onAdd } = props;
   return (
     <main className="appointment__add">
-      <img className="appointment__add-button" src="images/add.png" alt="Add" />
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={onAdd}
+      />
     </main>
   );
 };
+
+export default Empty;
