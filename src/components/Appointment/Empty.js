a2702a62b8e70918996b5146949ad77e459d151d
Sam Maisha <sam.maisha99@gmail.com>
2023-03-27 12:00:11 -0400

    create <Empty> component which shows button user can click on to add an appointment; import into index.js

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,10 @@
+import { AddonStore } from "@storybook/addons";
+import React from "react";
+
+export default Empty = function (props) {
+  return (
+    <main className="appointment__add">
+      <img className="appointment__add-button" src="images/add.png" alt="Add" />
+    </main>
+  );
+};
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "components/Appointment/styles.scss";
 
 import Header from "./Header";
+import Empty from "./Empty";
 
 const Appointment = (props) => {
   return <article className="appointment"></article>;
