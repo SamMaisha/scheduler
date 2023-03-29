d8a750f446566614e1a814fabf79bdbaa96cef9f
Sam Maisha <sam.maisha99@gmail.com>
2023-03-28 20:29:46 -0400

    fix onSubmit bug in form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -15,14 +15,13 @@ const Form = (props) => {
 
   const cancel = () => {
     reset();
-    onCancel;
-    Form(props);
+    onCancel();
   };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -40,7 +39,7 @@ const Form = (props) => {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>
+          <Button danger onClick={() => cancel()}>
             Cancel
           </Button>
           <Button confirm onClick={onSave}>
