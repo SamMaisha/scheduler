4826a8c274e094c8487509c302f7e5ee16cfa9fb
Sam Maisha <sam.maisha99@gmail.com>
2023-04-01 20:48:29 -0400

    get days using axios and setDays using response data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./Daylist";
 import Appointment from "./Appointment";
 
 /////////////////////// DATA /////////////////////////
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -66,7 +49,16 @@ const appointments = {
 ///////////// COMPONENT //////////////////////
 
 export default function Application(props) {
+  // fetching days from API
+  const [days, setDays] = useState([]);
+
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      setDays(response.data);
+    });
+  }, []);
 
   const appointmentArr = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
