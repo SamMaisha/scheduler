8c9b163d295a51542d1ffb1ab4042e5a8d3531d9
Sam Maisha <sam.maisha99@gmail.com>
2023-04-05 12:41:54 -0400

    add tests for setting of the inital EMPTY mode

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,3 +1,12 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
-import useVisualMode from "hooks/useVisualMode";
+import { useVisualMode } from "hooks/useVisualMode";
+
+// test EMPTY state
+const FIRST = "FIRST";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
