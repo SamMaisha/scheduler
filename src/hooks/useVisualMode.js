86e90c830deb8443de6fdbd817cea72c7435d7c8
Sam Maisha <sam.maisha99@gmail.com>
2023-04-05 13:03:46 -0400

    implement transition function and ensure it passes tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
-export function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
-  return { mode };
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+
+  const transition = (next) => {
+    setMode(next);
+  };
+
+  return { mode, transition };
 }
