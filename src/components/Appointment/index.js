31b8ba438913dd0413494c7eae8061cbb971c813
Sam Maisha <sam.maisha99@gmail.com>
2023-04-07 11:17:03 -0400

    import useVisualMode, and declare EMPTY and SHOW variables

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
-
+import { useVisualMode } from "hooks/useVisualMode";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 
 const Appointment = (props) => {
   const { time, interview } = props;
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
   return (
     <article className="appointment">
       <Header time={time} />
