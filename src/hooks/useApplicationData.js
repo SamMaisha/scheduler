86a8d5766dcaf7938a4b005ed8bd0c2423b995c5
Sam Maisha <sam.maisha99@gmail.com>
2023-04-10 14:19:52 -0400

    copy paste logic to move out from <Appointment> into useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,74 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useApplicationData() {
+  // state data
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+  //Fetch data from API and set state
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
+  }, []);
+  const setDay = (day) => setState({ ...state, day });
+  /**
+   *
+   * @param {number} id -> appointment id
+   * @param {object} interview
+   * @returns a promise and sets the state for appointments
+   */
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    return axios.put(`/api/appointments/${id}`, appointment).then(() => {
+      setState({
+        ...state,
+        appointments,
+      });
+    });
+  }
+  /**
+   *
+   * @param {number} id
+   * @returns a promise and sets the state for appointments
+   */
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      setState({
+        ...state,
+        appointments,
+      });
+    });
+  }
+
   return {
     state,
     setDay,
