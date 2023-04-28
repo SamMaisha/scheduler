5f6c17759697021de133bf133822ced96904b06e
Sam Maisha <sam.maisha99@gmail.com>
2023-04-28 08:04:58 -0400

    render form and destructure getPlacerHolderText for first test; pass fake interviewer data to Form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -14,6 +14,9 @@ describe("Form", () => {
   ];
 
   it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(
+      <Form interviewers={interviewers} />
+    );
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
