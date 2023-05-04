40ecf54d548ed49b8bf06608de0167d63592010d
Sam Maisha <sam.maisha99@gmail.com>
2023-05-04 13:35:22 -0400

    add the command to type the name 'Lydia Miller-Jones' into the student input field

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,6 +2,7 @@ describe("appointments", () => {
   it("should book an interview", () => {
     cy.visit("/");
     cy.contains("Monday");
-    cy.get(["alt=Add"]).first().click();
+    cy.get("[alt=Add]").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
   });
 });
