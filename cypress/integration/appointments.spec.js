b1ae14a8b4313a0db15a432603b12533e4a86b4c
Sam Maisha <sam.maisha99@gmail.com>
2023-05-04 15:20:08 -0400

    add test for editing an existing appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -12,4 +12,15 @@ describe("appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+  it("should edit an interview", () => {
+    cy.contains("[data-testid=appointment]", "Archie Cohen");
+    cy.get("[alt=Edit]").click({ force: true });
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
