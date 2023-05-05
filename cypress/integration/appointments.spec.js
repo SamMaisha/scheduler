8e68caa58ae1181ba4a3514b91a17c0f7ff0416b
Sam Maisha <sam.maisha99@gmail.com>
2023-05-05 08:01:55 -0400

    add test to delete an existing appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,4 +22,11 @@ describe("appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
+  it("should delete an interview", () => {
+    cy.get("[alt=Delete]").first().click({ force: true });
+    cy.contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
 });
